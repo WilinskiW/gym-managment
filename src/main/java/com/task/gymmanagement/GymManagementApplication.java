7aab71da1030f60fadc3e6cc65c8d9ae6b30a157
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-05-05 19:28:49 +0200

    Fix management typo

--- src/main/java/com/task/gymmanagement/GymManagementApplication.java
+++ src/main/java/com/task/gymmanagement/GymManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GymManagmentApplication {
+public class GymManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GymManagmentApplication.class, args);
+        SpringApplication.run(GymManagementApplication.class, args);
     }
 
 }
