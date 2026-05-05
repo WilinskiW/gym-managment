1b26fd495b8d7b4ecf21f8799a2382e2f56afc6b
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-05-05 21:37:59 +0200

    Refactor GlobalExceptionHandler

--- src/main/java/com/task/gymmanagement/infrastructure/error/GlobalExceptionHandler.java
+++ src/main/java/com/task/gymmanagement/infrastructure/error/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.task.gymmanagement.infrastructure.error;
 
+import com.task.gymmanagement.domain.exception.GymAlreadyExistException;
 import com.task.gymmanagement.domain.exception.GymNotFoundException;
+import com.task.gymmanagement.domain.exception.MemberNotFoundException;
+import com.task.gymmanagement.domain.exception.MembershipPlanExceedLimitException;
+import com.task.gymmanagement.domain.exception.MembershipPlanNotFoundException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +16,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 @Log4j2
 public class GlobalExceptionHandler {
+    private static final String DEFAULT_ERROR_MESSAGE = "An unexpected error occurred";
     private static final String DEFAULT_VALIDATION_MESSAGE = "Validation failed";
     private static final String JSON_PARSE_ERROR_MESSAGE = "Invalid JSON format or value";
     private static final String VALIDATION_ERROR_LOG_MESSAGE = "Validation error: {}";
     private static final String JSON_PARSE_LOG_MESSAGE = "JSON parse error: {}";
     private static final String ILLEGAL_ARGUMENT_LOG_MESSAGE = "Illegal argument: {}";
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGeneralException(Exception ex) {
+        log.error(DEFAULT_ERROR_MESSAGE, ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred"));
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException ex) {
@@ -42,9 +54,23 @@ public class GlobalExceptionHandler {
                 .body(new ErrorResponseDto(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
     }
 
-    @ExceptionHandler(GymNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handleGymNotFoundException(GymNotFoundException ex) {
+    @ExceptionHandler({GymNotFoundException.class, MemberNotFoundException.class, MembershipPlanNotFoundException.class})
+    public ResponseEntity<ErrorResponseDto> handleNotFoundException(RuntimeException ex) {
+        return handleException(ex, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({MembershipPlanExceedLimitException.class})
+    public ResponseEntity<ErrorResponseDto> handleMembershipPlanExceedLimitException(RuntimeException ex) {
+        return handleException(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(GymAlreadyExistException.class)
+    public ResponseEntity<ErrorResponseDto> handleGymAlreadyExistException(RuntimeException ex) {
+        return handleException(ex, HttpStatus.CONFLICT);
+    }
+
+    private ResponseEntity<ErrorResponseDto> handleException(RuntimeException ex, HttpStatus status) {
         log.warn(ex.getMessage());
-        return new ResponseEntity<>(new ErrorResponseDto(HttpStatus.NOT_FOUND.value(), ex.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponseDto(status.value(), ex.getMessage()), status);
     }
 }
